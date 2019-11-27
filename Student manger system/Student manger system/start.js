29f77c012063dfec08d94739ce5f1c8f80168210
hello413 <2827717859@qq.com>
2019-11-27 23:12:59 +0800

    practical

--- Student manger system/Student manger system/start.js
+++ Student manger system/Student manger system/start.js
@@ -2,20 +2,19 @@ const http=require("http");
 const url=require("url");
 const querystring = require('querystring');
 http.createServer(function(req,res){
-    res.setHeader('access-control-allow-origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*"); 
     var pathname=url.parse(req.url).pathname;
     var query = url.parse(req.url).query;
     
     if(pathname=='/test'){
         var postData = "";
-        req.on("data", function (getDataChunk) {
-            postData += getDataChunk;
-          });
+        req.on("data", function (postDataChunk) {
+            postData += postDataChunk;
+        });
           
         req.on("end",function(){
             var params = querystring.parse(postData);
-            var a=params.name;
-            console.log(a);
+            console.log(params.name);
             res.write("收到了");
             res.end();
         })
